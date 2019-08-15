5995fe6c89a6dc0fe34b5683495780fdef02346a
codedeedope <26957536+codedeedope@users.noreply.github.com>
2019-08-15 10:29:51 +0200

    refactoring of vicon_bridge.cpp: has a header file now

---
+++ src/vicon_bridge/src/vicon_bridgeNodeMain.cpp
@@ -0,0 +1,17 @@
+/*
+#include "vicon_bridge.cpp"
+
+int main(int argc, char** argv)
+{
+  ros::init(argc, argv, "vicon");
+//  ViconReceiver vr;
+//  ros::spin();
+
+  ros::AsyncSpinner aspin(1);
+  aspin.start();
+  ViconReceiver vr;
+  aspin.stop();
+  return 0;
+}
+*/
+
